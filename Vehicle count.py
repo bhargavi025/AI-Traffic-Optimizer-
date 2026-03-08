e7168b32642990fef605c505def3342c0b1db542
Bhargavi Newale <168109177+bhargavi025@users.noreply.github.com>
2026-03-08 06:14:25 +0530

    Add files via upload

---
+++ Vehicle count.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+import cv2
+
+# load model
+model = YOLO("C:\\Users\\Bhargavi\\Documents\\yolov8m.pt")
+
+# read image
+image = cv2.imread("D:\\traffic1.jpg")
+
+# run detection
+results = model(image)
+
+car_count = 0
+truck_count = 0
+bike_count = 0
+bus_count = 0
+
+for cls in results[0].boxes.cls:
+    cls = int(cls)
+
+    if cls == 2:      # car
+        car_count += 1
+    elif cls == 7:    # truck
+        truck_count += 1
+    elif cls == 3:
+        bike_count += 1
+    elif cls == 5:
+        bus_count +=1
+
+total = car_count + truck_count + bike_count +bus_count
+
+print("Cars:", car_count)
+print("Trucks:", truck_count)
+print("Bikes:", bike_count)
+print("Total Vehicles:", total)
