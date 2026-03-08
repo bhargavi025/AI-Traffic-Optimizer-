95566382727a0afa0fa26d3462e4b2f9e02daa26
Bhargavi Newale <168109177+bhargavi025@users.noreply.github.com>
2026-03-08 06:27:25 +0530

    Ambulance detection.py

--- Ambulance detection.py
+++ Ambulance detection.py
@@ -3,11 +3,11 @@ from ultralytics import YOLO
 
 def main():
     """
-    model = YOLO("C:\\Users\\Bhargavi\\Documents\\yolov8m.pt")
+    model = YOLO("yolov8m.pt")
 
-    results = model.train(data="C:\\Users\\Bhargavi\\Documents\\data.yaml", epochs=50,batch=16,conf=0.2,workers=0,save=True)
+    results = model.train(data="data.yaml", epochs=50,batch=16,conf=0.2,workers=0,save=True)
     """
-    model1 = YOLO("D:\\YOLO\\runs\\detect\\train5\\weights\\best.pt")
+    model1 = YOLO("weights\\best.pt")
     
     result = model1.predict(source="D:\\ambulance2.jpg",conf=0.5,iou=0.4,classes=[0,1],save=True) 
     
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     import multiprocessing
     multiprocessing.freeze_support()  # For safety on Windows
     main()
+
 
 
 
